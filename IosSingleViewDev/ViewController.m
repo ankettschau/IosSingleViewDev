e9de18f34473ce6f2ad294322f0b95c35ffe9ac4
Jochen1980 <jochen.bauer@gmail.com>
2014-01-03 21:35:36 +0100

    first

--- IosSingleViewDev/ViewController.m
+++ IosSingleViewDev/ViewController.m
@@ -9,21 +9,55 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *header;
+@property (weak, nonatomic) IBOutlet UITextField *inputUsername;
+@property (weak, nonatomic) IBOutlet UITextField *inputPassword;
+@property NSString *storedUsername;
+@property NSString *storedPassword;
+@property NSString *username;
+@property NSString *password;
+
+- (id)init;
+- (IBAction)enterButton:(id)sender;
+- (BOOL)checkLogin:(NSString*)name withPassword:(NSString*) password;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+-(id)init {
+    self = [super init];
+    if (self != nil){
+        self.storedUsername = @"User";
+        self.storedPassword = @"Pass";
+    }
+    return self;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidLoad {
+    [super viewDidLoad];
 }
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+- (IBAction)enterButton:(id)sender {
+    NSLog(@"BP0");
+    BOOL isLoggedIn = [self checkLogin:self.username withPassword:self.password];
+    if (isLoggedIn){
+        self.header.text = @"Profil";
+        NSLog(@"BP9");
+    }
+}
+
+- (BOOL)checkLogin:(NSString*)name withPassword:(NSString*) password {
+    NSLog(@"BP0");
+    if ((name == self.storedUsername) && (password == self.storedPassword)){
+        return true;
+    }
+    return false;
+}
+
 
 @end
